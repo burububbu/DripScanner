77ab5d3ce710238b797e6db82babe5d2faede2df
Alessandra Boccuto <aless97b@gmail.com>
2019-08-18 20:13:36 +0200

    fixed ownership rules

--- backend/src/services/owners/owners.service.ts
+++ backend/src/services/owners/owners.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Owner } from './Owner.interface';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class OwnersService {
@@ -13,7 +14,11 @@ export class OwnersService {
     return await this.ownerModel.findOne({ owner: username }).exec();
   }
 
+  // rimuove l'ownership della drip nel caso in cui questa sia di un altro utente
   async addDrip(username: string, code: string) {
+    const oldOwner = await this.ownerModel.findOne({ drips: code }).exec();
+    this.removeDrip(oldOwner.owner, code);
+
     return await this.ownerModel
       .updateOne({ owner: username }, { $push: { drips: code } })
       .exec();
