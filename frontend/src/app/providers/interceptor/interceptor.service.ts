fd531afc2a06b5453e2d98650b0adb28f4592b93
Alessandra Boccuto <aless97b@gmail.com>
2019-09-04 19:23:17 +0200

    modified interceptor

--- frontend/src/app/providers/interceptor/interceptor.service.ts
+++ frontend/src/app/providers/interceptor/interceptor.service.ts
@@ -1,49 +1,21 @@
 import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { AlertController } from "@ionic/angular";
 import { AuthService } from "./../auth/auth.service";
-import { Observable, from, throwError } from "rxjs";
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent
-} from "@angular/common/http";
-import { mergeMap, catchError } from "rxjs/operators";
+import { Observable, from } from "rxjs";
+import { HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { mergeMap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class InterceptorService {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly alertCtrl: AlertController,
-    private readonly router: Router
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return from(this.authService.access_token).pipe(
-      mergeMap(token =>
-        next.handle(this.addToken(req, token)).pipe(
-          catchError(e => {
-            if (e.status === 403) {
-              this.router.navigate(["/unauthorized"]);
-            } else {
-              this.alertCtrl
-                .create({
-                  header: e.name,
-                  message: e.message,
-                  buttons: ["OK"]
-                })
-                .then(a => a.present());
-            }
-            return throwError(e);
-          })
-        )
-      )
+      mergeMap(token => next.handle(this.addToken(req, token)))
     );
   }
 
