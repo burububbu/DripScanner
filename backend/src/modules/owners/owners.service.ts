1fe67adc2824c63b85c3d572a2631463766cdf77
Alessandra Boccuto <aless97b@gmail.com>
2019-09-06 08:57:42 +0200

    modified date

--- backend/src/modules/owners/owners.service.ts
+++ backend/src/modules/owners/owners.service.ts
@@ -79,7 +79,7 @@ export class OwnersService {
     if (drip.shareable === false) {
       throw new ForbiddenException('The drip is not shareable');
     }
-    if (drip.expireDate && drip.expireDate <= new Date()) {
+    if (drip.expireDate && new Date(drip.expireDate.toUTCString()) <= new Date(new Date().toUTCString())) {
       await this.setState(owner.owner, drip.id, false);
       throw new ForbiddenException('The drip is not shareable anymore');
     }
